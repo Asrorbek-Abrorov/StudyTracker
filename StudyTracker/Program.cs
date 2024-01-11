f0b42d7c89161cd3a3f70c4c31defc8bb7749bbf
Asrorbek <as.abrorov@gmail.com>
2024-01-11 20:50:14 +0500

    namespace changed

--- StudyTracker/Program.cs
+++ StudyTracker/Program.cs
@@ -3,19 +3,18 @@ using StudyTracker.Entities;
 using StudyTracker.Services;
 using StudyTracker.Uis;
 
-namespace StudyTracker
-{
-    public class Program
-    {
-        static void Main()
-        {
-            var courseService = new CourseService();
-            var studyPlanService = new StudyPlanService();
-            var progressRecordService = new ProgressRecordService();
-            var studentService = new StudentService();
+namespace StudyTracker;
 
-            var mainUi = new MainUi(courseService, studyPlanService, progressRecordService, studentService);
-            mainUi.Run();
-        }
+public class Program
+{
+    static void Main()
+    {
+        var courseService = new CourseService();
+        var studyPlanService = new StudyPlanService();
+        var progressRecordService = new ProgressRecordService();
+        var studentService = new StudentService();
+
+        var mainUi = new MainUi(courseService, studyPlanService, progressRecordService, studentService);
+        mainUi.Run();
     }
 }
